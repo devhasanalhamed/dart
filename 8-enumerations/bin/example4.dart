492c637cdb38b995df2befaa44f67e160c382d1b
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-02-27 10:58:42 +0300

    Update example4.dart

--- 8-enumerations/bin/example4.dart
+++ 8-enumerations/bin/example4.dart
@@ -1,9 +1,32 @@
 void main(List<String> args) {
   /// [converting] strings to enumerations
-  print('fsdf');
+  /// this is very useful when we have a lot of json and we do not need
+  /// a lot of json parsing
+  describe(animalType(fromStr: 'cat'));
+  describe(animalType(fromStr: 'dog'));
+  describe(animalType(fromStr: 'rabbit'));
+  describe(animalType(fromStr: 'corn'));
 }
 
-AnimalType? animalType({required String fromStr}) =>
-    AnimalType.values.firstWhere((animal) => );
+void describe(AnimalType? animalType) {
+  switch (animalType) {
+    case AnimalType.rabbit:
+      print('This is rabbit');
+    case AnimalType.dog:
+      print('This is dog');
+    case AnimalType.cat:
+      print('This is cat');
+    default:
+      print('Unknown animal');
+  }
+}
+
+AnimalType? animalType({required String fromStr}) {
+  try {
+    return AnimalType.values.firstWhere((animal) => animal.name == fromStr);
+  } catch (e) {
+    return null;
+  }
+}
 
 enum AnimalType { rabbit, dog, cat }
