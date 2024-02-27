5cc038eaefe648c56987b12334cf54759932b4f7
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-02-27 10:22:55 +0300

    Create example4.dart

---
+++ 8-enumerations/bin/example4.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  /// [converting] strings to enumerations
+  print('fsdf');
+}
+
+AnimalType? animalType({required String fromStr}) =>
+    AnimalType.values.firstWhere((animal) => );
+
+enum AnimalType { rabbit, dog, cat }
