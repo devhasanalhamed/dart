20b844697f961e6f5a6ab4bff9f168300a3209f0
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-03-04 11:07:54 +0300

    Limiting mixins to data types

--- 10-mixins/bin/example4.dart
+++ 10-mixins/bin/example4.dart
@@ -1,3 +1,28 @@
 void main(List<String> args) {
   /// [Limiting] mixins to data types
+  Human().run();
 }
+
+class HasTwoFeet {
+  const HasTwoFeet();
+}
+
+/// now this is a mixin on a class not an another mixin
+/// so if we want to allow the mixin to be on our class
+/// we should create a sub class from has two feet and implement with canRun
+
+mixin CanRun on HasTwoFeet {
+  void run() => print('$runtimeType is running');
+
+  /// can run is available on any type that has two feet if that type
+  /// bring it in, so it's only available for has two feet
+}
+
+class HasNoFeet {
+  const HasNoFeet();
+}
+
+/// this is the implementation of a class and a mixin that is on it
+class Human extends HasTwoFeet with CanRun {}
+
+class Fish extends HasNoFeet {}
