786233aa766fe9bbf7b0d64c94daee2b2cb666fe
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-03-13 13:25:02 +0300

    Create example6.dart

---
+++ 12-extensions/bin/example6.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  /// Extending enums
+  print(AnimalType.cat.nameContainsUpperCaseLetters);
+  print(AnimalType.dog.nameContainsUpperCaseLetters);
+  print(AnimalType.goldFish.nameContainsUpperCaseLetters);
+}
+
+enum AnimalType {
+  cat,
+  dog,
+  goldFish,
+}
+
+extension on Enum {
+  bool get nameContainsUpperCaseLetters => name.contains(RegExp(r'[A-Z]'));
+}
