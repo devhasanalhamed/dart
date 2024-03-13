8fe0b4d0a285293c2b3b454fa24de2ad88a64b30
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-03-13 13:24:58 +0300

    Create example4.dart

---
+++ 12-extensions/bin/example4.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  /// Finding duplicate in values in iterable
+  print([1, 2, 3].containsDuplicateValues);
+  print([1, 2, 3, 1].containsDuplicateValues);
+  print(['Foo', 'Bar'].containsDuplicateValues);
+  print(['Foo', 'Bar', 'foo'].containsDuplicateValues);
+  print(['Foo', 'Bar', 'Foo'].containsDuplicateValues);
+}
+
+extension on Iterable {
+  bool get containsDuplicateValues => toSet().length != length;
+}
