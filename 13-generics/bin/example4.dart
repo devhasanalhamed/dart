c7b6583a8c3dcb3b158f36b90b00d8978dfd60bd
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-03-16 12:55:45 +0300

    Update example4.dart

--- 13-generics/bin/example4.dart
+++ 13-generics/bin/example4.dart
@@ -1,4 +1,14 @@
 void main(List<String> args) {
   /// Unconstrained generic type definitions
-  
+  final one = KeyEntry(1, 2); // MapEntry<int, int>
+  print(one);
+  final two = KeyEntry(1, 2.5); // MapEntry<int, double>
+  print(two);
+  final three = KeyEntry(1, 'Foo'); // MapEntry<int, String>
+  print(three);
+  final KeyEntry four = KeyEntry('address', null); // MapEntry<dynamic, dynamic>
+  print(four);
 }
+
+/// if we don't use generic types, dart wouldn't understand our datatype
+typedef KeyEntry<K, V> = MapEntry<K, V>;
