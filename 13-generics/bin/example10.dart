0ca012d6e0199575e62ff131ccf2cff8a86ce40a
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-03-18 13:36:49 +0300

    Create example10.dart

---
+++ 13-generics/bin/example10.dart
@@ -0,0 +1,58 @@
+void main(List<String> args) {
+  final personName =
+      personThing.mapIfOfType((Person p) => p.name) ?? 'Unknown person name';
+  print(personName);
+
+  final animalName =
+      animalThing.mapIfOfType((Animal a) => a.name) ?? 'Unknown animal name';
+  print(animalName);
+
+  final unknownAnimal =
+      personThing.mapIfOfType((Animal a) => a.name) ?? 'Unknown animal name';
+  print(unknownAnimal);
+}
+
+abstract class Thing {
+  final String name;
+  const Thing({required this.name});
+}
+
+class Person extends Thing {
+  final int age;
+
+  const Person({
+    required String name,
+    required this.age,
+  }) : super(name: name);
+}
+
+class Animal extends Thing {
+  final String species;
+
+  const Animal({
+    required String name,
+    required this.species,
+  }) : super(name: name);
+}
+
+const Thing personThing = Person(
+  name: 'Foo',
+  age: 20,
+);
+
+const Thing animalThing = Animal(
+  name: 'Bar',
+  species: 'Cat',
+);
+
+/// this extension will run on any datatype
+extension<T> on T {
+  R? mapIfOfType<E, R>(R Function(E) f) {
+    final shadow = this;
+    if (shadow is E) {
+      return f(shadow);
+    } else {
+      return null;
+    }
+  }
+}
