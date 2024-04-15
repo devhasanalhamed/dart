229a2508987f9b515b4e9a03f11a656ed7ad183a
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-15 16:01:32 +0300

    Update example11.dart

--- 15-asynchronous_programming/bin/example11.dart
+++ 15-asynchronous_programming/bin/example11.dart
@@ -1,3 +1,14 @@
-void main(List<String> args) {
+void main(List<String> args) async {
   /// Stream to [list]
+  /// to list function will wait for the stream to finish
+  final allNames = await getNames().toList();
+  print(allNames);
+}
+
+Stream<String> getNames() async* {
+  yield 'John';
+  await Future.delayed(Duration(seconds: 1));
+  yield 'Jane';
+  await Future.delayed(Duration(seconds: 1));
+  yield 'Jack';
 }
