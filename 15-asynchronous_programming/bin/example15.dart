92e323b57ceb2205f0ba078d4d143b43550c8fc4
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-16 14:39:52 +0300

    Update example15.dart

--- 15-asynchronous_programming/bin/example15.dart
+++ 15-asynchronous_programming/bin/example15.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   /// [Broadcast] streams, a stream is an object can be listened once at a time
-  /// that's why we have two types regular stream and broadcast which can be listened from multiple listeners at the same time
+  /// that's why we have two types non-broadcast stream and broadcast which can be listened from multiple listeners at the same time
+  await nonBroadcastStreamExample();
+
+  await broadcastStreamExample();
 }
 
 Future<void> nonBroadcastStreamExample() async {
@@ -10,4 +13,40 @@ Future<void> nonBroadcastStreamExample() async {
   controller.sink.add('Bob');
   controller.sink.add('Alice');
   controller.sink.add('Joe');
+
+  try {
+    await for (final name in controller.stream) {
+      print(name);
+      await for (final name in controller.stream) {
+        print(name);
+      }
+    }
+  } catch (e) {
+    print(e);
+  }
+}
+
+Future<void> broadcastStreamExample() async {
+  late final StreamController<String> controller;
+
+  controller = StreamController<String>.broadcast();
+
+  final sub1 = controller.stream.listen((name) {
+    print('sub1: $name');
+  });
+
+  final sub2 = controller.stream.listen((name) {
+    print('sub2: $name');
+  });
+
+  controller.sink.add('Bob');
+  controller.sink.add('Alice');
+  controller.sink.add('Joe');
+  controller.close();
+
+  controller.onCancel = () {
+    print('onCancel');
+    sub1.cancel();
+    sub2.cancel();
+  };
 }
