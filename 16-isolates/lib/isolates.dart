5e96b998d7a82319ba1e9fb12ae0ff916fb2cd69
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-17 14:36:11 +0300

    Update isolates.dart

--- 16-isolates/lib/isolates.dart
+++ 16-isolates/lib/isolates.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:isolate';
+
+void main(
+  List<String> args,
+  SendPort message,
+) async {
+  /// [Spawning] isolates using a uri
+  message.send('Hello World!');
 }
