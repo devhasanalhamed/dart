6c14d358db88e552caf4c76586a9e187668dc33d
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2024-07-29 14:28:24 +0300

    Update assignments.dart

--- assignments/bin/assignments.dart
+++ assignments/bin/assignments.dart
@@ -1,13 +1,25 @@
-void main(List<String> arguments) {
-  String text = "Ha%a0 Al-Hamed Un#cod~ 🦄";
-
-  print(text.replaceChar(2, "s"));
+void main(List<String> arguments) async {
+  try {
+    await for (final event in simpleEmitter([1, 2, 3, 4, 5])) {
+      print("Main: $event");
+    }
+  } catch (e) {
+    print(e);
+  }
 }
 
-extension ReplaceIt on String {
-  String replaceChar(int index, String char) {
-    List<String> list = split('');
-    list[index] = char;
-    return list.join();
+Stream<String> simpleEmitter(List<int> numbersToPrint) async* {
+  for (final number in numbersToPrint) {
+    await Future.delayed(Duration(seconds: 1));
+    await for (final string in simpleStringEmitter(["One", "Two"])) {
+      yield "SimpleEmitter: $string";
+    }
+  }
+}
+
+Stream<String> simpleStringEmitter(List<String> stringsToPrint) async* {
+  for (final string in stringsToPrint) {
+    await Future.delayed(Duration(seconds: 1));
+    yield "SimpleStringEmitter: $string";
   }
 }
